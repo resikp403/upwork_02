f3a2f4976e568c903834306013ed2b07382ff000
sapar0929 <sprmrt.glm@gmail.com>
2025-09-23 20:00:58 +0500

    1.4.3

---
+++ app/Models/Visitor.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Visitor extends Model
+{
+    protected $guarded = ['id'];
+
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
+
+    public function ipAddress(): BelongsTo
+    {
+        return $this->belongsTo(IpAddress::class);
+    }
+
+    public function userAgent(): BelongsTo
+    {
+        return $this->belongsTo(UserAgent::class);
+    }
+}
